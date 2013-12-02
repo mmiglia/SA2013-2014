5d1e4e71208dbe62613856728e1b9cfedf3904eb
mmiglia <mauro.migliardi@unipd.it>
2013-12-02 15:51:28 +0100

    Fine lezione 2/12/2013

---
+++ net/Request.java
@@ -0,0 +1,36 @@
+package net;
+
+public class Request
+implements java.io.Serializable
+{
+	static int count = 0;
+	int num;
+	boolean complete;
+
+	public Request()
+	{
+		synchronized(Request.class)
+		{
+			num = count++;
+		}
+		complete = false;
+	}
+
+	public void execute()
+	{
+		System.out.println("Richiesta #"+num+" eseguita");
+		complete = true;
+	}
+
+	public String toString()
+	{
+		if(complete)
+		{
+			return "Request #" + num + " is complete";
+		}
+		else
+		{
+			return "Request #" + num + " is incomplete";
+		}
+	}
+}
---
+++ net/ServerMT.java
@@ -0,0 +1,176 @@
+package net;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ServerMT
+{
+	ServerSocket ss = null;
+	List<Envelope> q = null;
+
+	public ServerMT()
+	{
+		q = new LinkedList<Envelope>();
+	}
+	
+	public static void main(String[] argv)
+	{
+		ServerMT smt = new ServerMT();
+		smt.exec(argv);
+	}
+
+	void exec(String[] argv)
+	{
+		try
+		{
+			ss = new ServerSocket(5678);
+		}
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+			System.exit(1);
+		}
+
+		while(true)
+		{
+			try
+			{
+				System.out.print("In attesa di connessione....");
+				Socket s = ss.accept();
+				System.out.println("Connesso!");
+
+				Listener l = new Listener(s, q);
+				(new Thread(l)).start();
+			}
+			catch(IOException ioe)
+			{
+				ioe.printStackTrace();
+			}
+		}
+	}
+
+	class Listener
+	implements Runnable
+	{
+		Socket s;
+		List<Envelope> q;
+
+		Listener(Socket theS, List<Envelope> theQ)
+		{
+			s = theS;
+			q = theQ;
+		}
+
+		public void run()
+		{
+				try
+				{
+					System.out.println("con " + s.getInetAddress().toString() + " sulla porta " + s.getPort());
+	
+					InputStream is = s.getInputStream();
+					OutputStream os = s.getOutputStream();
+
+					ObjectInputStream ois = new ObjectInputStream(is);
+					ObjectOutputStream oos = new ObjectOutputStream(os);
+
+					try
+					{
+						while(true)
+						{
+							Request req = (Request)ois.readObject();
+							synchronized(q)
+							{
+								q.add(new Envelope(req, oos));
+								q.notify();
+							}
+						}
+					}
+					catch(ClassNotFoundException cnfe)
+					{
+						cnfe.printStackTrace();
+					}
+					catch(IOException ioe)
+					{
+						ioe.printStackTrace();
+					}
+					finally
+					{
+						ois.close();
+						oos.close();
+						s.close();
+						return;
+					}
+			}
+			catch(IOException ioe)
+			{
+				ioe.printStackTrace();
+			}
+		}
+	}
+
+	class Servant
+	implements Runnable
+	{
+		List<Envelope> q;
+
+		Servant(List<Envelope> theQ)
+		{
+			q = theQ;
+		}
+
+		public void run()
+		{
+			while(true)
+			{
+				Envelope e = null;
+				synchronized(q)
+				{
+					while(q.isEmpty())
+					{
+						try
+						{
+							q.wait();
+						}
+						catch(InterruptedException ie)
+						{
+							ie.printStackTrace();
+						}
+					}
+					e = q.remove(0);
+				}
+
+				System.out.println(Thread.currentThread().getName() + " Ho preso in carico la richiesta <" + e.r.toString() + ">");
+
+				//esegue qualunque cosa ci sia da fare
+				e.r.execute();
+
+				//restituisce la risposta
+				synchronized(e.oos)
+				{
+					try
+					{
+						e.oos.writeObject(e.r);
+						e.oos.flush();
+					}
+					catch(IOException ioe)
+					{
+						ioe.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+
+	class Envelope
+	{
+		Request r;
+		ObjectOutputStream oos;
+
+		Envelope(Request theR, ObjectOutputStream theOOS)
+		{
+			r = theR;
+			oos = theOOS;
+		}
+	}
+}
